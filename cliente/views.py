ba01c8dcf3aca9880e9713207e52bee6fcbcb29f
Gabriel Preti Ferraz <125829214+gabriel-preti-ferraz@users.noreply.github.com>
2023-04-15 20:24:52 -0300

    Ajuste de login

--- cliente/views.py
+++ cliente/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from eventos.models import Certificado, Evento
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def meus_certificados(request):
     certificados = Certificado.objects.filter(participante=request.user)
     return render(request, 'meus_certificados.html', {'certificados':certificados})
 
+@login_required
 def meus_eventos(request):
     if request.method == "GET":
         nome = request.GET.get('nome')
