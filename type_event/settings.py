8987b18c0048b5b448ce4c201c06f311ba84aaa1
Gabriel Preti Ferraz <125829214+gabriel-preti-ferraz@users.noreply.github.com>
2023-04-15 21:09:45 -0300

    Feito landing page (simples)

--- type_event/settings.py
+++ type_event/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'usuarios',
     'eventos',
     'cliente',
+    'inicio',
 ]
 
 MIDDLEWARE = [
--- type_event/urls.py
+++ type_event/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/', include('inicio.urls')),
     path('usuarios/', include('usuarios.urls')),
     path('eventos/', include('eventos.urls')),
     path('cliente/', include('cliente.urls'))
